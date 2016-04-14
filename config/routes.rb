e9d4dfe0d071d4088aae2a9d9a147675d5ab2b66
kuroun <roun.sk@gmail.com>
2016-04-14 18:48:57 -0500

    I1: Add home controller and design layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :bookings
 
   resources :recording_sessions
 
   resources :users
   
-  root 'bookings#index'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

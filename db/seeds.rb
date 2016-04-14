fac339428d1ecee2dbf374f3381a8a972e2f5d90
kuroun <roun.sk@gmail.com>
2016-04-14 17:23:38 -0500

    I1: seeding data user and recording session

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+session1 = {start_time: '2000-01-01 09:00:00 UTC', end_time: '2000-01-01 11:45:00 UTC', description: 'Morning Session'}
+session2 = {start_time: '2000-01-01 13:00:00 UTC', end_time: '2000-01-01 15:45:00 UTC', description: 'Afternoon Session 1'}
+session3 = {start_time: '2000-01-01 16:00:00 UTC', end_time: '2000-01-01 18:45:00 UTC', description: 'Afternoon Session 2'}
+session4 = {start_time: '2000-01-01 19:00:00 UTC', end_time: '2000-01-01 21:45:00 UTC', description: 'Evening Session'}
+RecordingSession.create([session1, session2, session3, session4])
+
+user1 = {band_name: 'The Beattles', contact_name: 'John', contact_number: '6414517127'}
+user2 = {band_name: 'F4', contact_name: 'Han Lee', contact_number: '641123456'}
+User.create([user1, user2])

aa8a37a78125ae40c704e698b8951c49356f3805
kuroun <roun.sk@gmail.com>
2016-04-15 00:01:41 -0500

    I1: validate and test 'Only one user may book a given recording session'

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,14 @@ class Booking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :recording_session
 	validates :user_id, :recording_session_id, :presence => true
-	#validates :user_id, :uniqueness => true
+	validate :only_one_user_booking_per_day
+	
+	def only_one_user_booking_per_day
+	  Booking.where(:recording_date => recording_date).each do |b|
+	    if(b.user_id == user_id)
+	      errors.add(:base,'Only one user can book a session per day')
+	      break
+	    end
+	  end
+	end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "allows only one user may book a given recording session" do
+    booking1 = Booking.create(:user_id => 1, :recording_session_id => 1,:recording_date => "2016-05-14")
+    booking2 = Booking.create(:user_id => 1, :recording_session_id => 2,:recording_date => "2016-05-14")
+    expect(booking2).to_not be_valid
+  end
 end

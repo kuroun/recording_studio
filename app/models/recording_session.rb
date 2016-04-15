ec4c564fade4697996ea4886aaa9d689bd4146db
kuroun <roun.sk@gmail.com>
2016-04-14 21:19:43 -0500

    I1: validate and test "Recording sessions are 2 hour 45 minutes long"

--- app/models/recording_session.rb
+++ app/models/recording_session.rb
@@ -1,3 +1,7 @@
 class RecordingSession < ActiveRecord::Base
 	has_many :bookings
+	validate :valid_duration
+	def valid_duration
+		errors.add(:base,'each session is 2 hour 45 minutes long') if ((end_time.to_i - start_time.to_i) > (2.hours.to_i + 45.minutes.to_i))
+	end
 end
--- spec/models/recording_session_spec.rb
+++ spec/models/recording_session_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RecordingSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'is 2 hour 45 minutes long' do
+    session = RecordingSession.create(:start_time => '09:00', :end_time => '12:00' )
+    expect(session).to_not be_valid
+  end
+ 
 end

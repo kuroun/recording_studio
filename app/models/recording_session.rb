0cd105cb69c770f73217c6bda7c43a6bc11f72b3
kuroun <roun.sk@gmail.com>
2016-04-14 23:44:02 -0500

    I1: validate and test 'The studio is closed for lunch from 12pm to 1pm daily'

--- app/models/recording_session.rb
+++ app/models/recording_session.rb
@@ -1,7 +1,11 @@
 class RecordingSession < ActiveRecord::Base
 	has_many :bookings
 	validate :valid_duration
+	validate :close_for_lunch
 	def valid_duration
 		errors.add(:base,'each session is 2 hour 45 minutes long') if ((end_time.to_i - start_time.to_i) > (2.hours.to_i + 45.minutes.to_i))
+	end
+	def close_for_lunch
+		errors.add(:start_time,'should not be between 12pm - 1pm') if(start_time.strftime('%H').to_i <= 13 && start_time.strftime('%H').to_i >= 12 )
 	end
 end
--- spec/models/recording_session_spec.rb
+++ spec/models/recording_session_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe RecordingSession, type: :model do
     session = RecordingSession.create(:start_time => '09:00', :end_time => '12:00' )
     expect(session).to_not be_valid
   end
- 
+  it 'closed for lunch from 12pm to 1pm daily' do
+    session1 = RecordingSession.create(:start_time => '12:00', :end_time => '01:30')
+    session2 = RecordingSession.create(:start_time => '12:30', :end_time => '02:00')
+    expect(session1).to_not be_valid
+    expect(session2).to_not be_valid
+  end
 end

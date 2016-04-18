6820d7af7e46b2ad927dd0ac5009e8eb904f358c
kuroun <roun.sk@gmail.com>
2016-04-17 22:36:37 -0500

    I2: testing scope retrieve available sessions

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ActiveRecord::Base
 	validates :user_id, :recording_session_id, :presence => true
 	validate :only_one_user_booking_per_day
 	
+
 	def only_one_user_booking_per_day
 	  Booking.where(:recording_date => recording_date).each do |b|
 	    if(b.user_id == user_id)
--- spec/models/recording_session_spec.rb
+++ spec/models/recording_session_spec.rb
@@ -18,4 +18,18 @@ RSpec.describe RecordingSession, type: :model do
     expect(session1).to_not be_valid
     expect(session2).to_not be_valid
   end
+  it 'should return available session for specific date' do
+    user = User.create(:band_name => 'F4', :contact_name => 'John', :contact_number => '1234567890')
+    room1 = Room.create(:room_name => "Room 1", :status => "used")
+    room2 = Room.create(:room_name => "Room 2", :status => "repaired")
+    room3 = Room.create(:room_name => "Room 3", :status => "used")
+    session1 = RecordingSession.create(:start_time => '9:00', :end_time => '11:45', :room_id => room1.id)
+    session2 = RecordingSession.create(:start_time => '9:00', :end_time => '11:45', :room_id => room2.id)
+    session3 = RecordingSession.create(:start_time => '9:00', :end_time => '11:45', :room_id => room3.id) 
+    date = '2016-04-20'.to_date
+    booking = Booking.create(:recording_date => date, :recording_session_id => session1.id, :user_id => user.id)
+    getAvailableRooms = RecordingSession.query_record_session(date)
+    expect(getAvailableRooms).to_not include([session1, session2])
+    expect(getAvailableRooms).to include(session3)     
+  end
 end
